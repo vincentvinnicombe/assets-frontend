181dea3b9477d341bcd6d47c00455d6d1dc87ca5
mattpepper <matt.pepper@outlook.com>
2015-05-30 22:30:56 +0100

    AOSS-329 Ajax behaviour for PAYE Client List page  "Ask for Access" form'   * update 'ajaxFormSubmit.js'      - to handle more than one button or form with 'data-ajax-submit="true" attribute      - to define container for result in "data-target-container" attribute      - to define "data-target-type" attribute giving the method of updating the container with result's partial view html (to 'replace' or 'insert')

--- assets/javascripts/modules/ajaxFormSubmit.js
+++ assets/javascripts/modules/ajaxFormSubmit.js
@@ -6,8 +6,11 @@
  * Usage:
  *
  *  Place the attribute 'data-ajax-submit="true"' on either a form tag or a button
- *  that has a formaction attribute for non-javascript enabled form post (full page reload)
- *  and has a data-formaction attribute for javascript enabled ajax post.
+ *  that has:
+ *    - a formaction attribute for non-javascript enabled form post (full page reload)
+ *    - a 'data-target-container' attribute with a selector for the element to target with new content
+ *    - a 'data-target-type' attribute to indicate the method to use when adding new content to the container - options are 'insert' or 'replace' (insert is the default)
+ *
  *
  *  <form action="#" data-ajax-submit="true">
  *      <input type="submit" value="Submit"/>
@@ -15,7 +18,7 @@
  *
  *  or
  *
- *  <button formaction="#" data-formaction="#" data-ajax-submit="true">Submit</button>
+ *  <button formaction="#" data-ajax-submit="true" data-target-type="" >Submit</button>
  *
  **/
 
@@ -26,34 +29,48 @@ var ajaxFormSubmit = {
   init: function() {
     var path,
         _this = this,
-        $ajaxForm = $('[data-ajax-submit]');
+        $ajaxForm = $('[data-ajax-submit]'),
+        formCount = $ajaxForm.length,
+        f = 0,
+        $ajaxItem = null;
 
-    if (!$ajaxForm.length) {
+    if (!formCount) {
       return false;
     }
 
-    $ajaxForm.parents('form').on('submit', function(event) {
-      event.preventDefault();
+    for (; f < formCount; f++) {
+      $ajaxItem = $($ajaxForm[f]);
 
-      if ($ajaxForm[0].nodeName.toLowerCase() === 'button') {
-        path = $ajaxForm.attr('formaction');
-      }
-      else {
-        path = $ajaxForm.attr('action');
-      }
+      $ajaxItem.parents('form').on('submit', { $form: $ajaxItem }, function(event) {
+        event.preventDefault();
 
-      _this.doSubmit(path, $(this).serialize() + '&isajax=true', $ajaxForm.attr('data-client'));
-    });
+        var $form = event.data.$form;
+
+        if ($form[0].nodeName.toLowerCase() === 'button') {
+          path = $form.attr('formaction');
+        }
+        else {
+          path = $form.attr('action');
+        }
+
+        _this.doSubmit(path, $(this).serialize() + '&isajax=true', $form.attr('data-target-container'), $form.attr('data-target-type'));
+      });
+    }
   },
 
-  doSubmit: function(path, data, cid) {
+  doSubmit: function(path, data, targetContainer, targetType) {
     $.ajax({
       url: path,
       type: 'POST',
       data: data
     })
     .done(function(result) {
-      $('#client_' + cid + '_notes').html(result);
+      if (targetType === 'replace') {
+        $(targetContainer).replaceWith(result);
+      } else {
+        // 'insert'
+        $(targetContainer).html(result);
+      }
     })
     .fail(function(result) {
       console.log('error', result);
